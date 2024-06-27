db9aad6724cb1670b62267bbb99f7be138d7b830
Niral Ajmera <102373223+niral261@users.noreply.github.com>
2024-06-27 09:52:45 +0530

    Update Hero.jsx

--- Downloads/portfolio/src/components/Hero.jsx
+++ Downloads/portfolio/src/components/Hero.jsx
@@ -1,139 +1,92 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { ComputersCanvas } from './canvas';
+import AnimatedText from './AnimatioText';
 
-const Computers = ({ isMobile }) => {
-  const { scene, error } = useGLTF("./desktop_pc/scene.gltf");
-
-  if (error) {
-    console.error("Error loading GLTF model:", error);
-    return null;
-  }
-
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.6} groundColor="violet" />
-      <pointLight intensity={1} position={[0, 10, 10]} />
-      <spotLight
-        position={[50, 10, 100]}
-        angle={0.5}
-        penumbra={1}
-        intensity={1}
-      />
-      <ambientLight intensity={0.5} />
-      <directionalLight 
-        intensity={1} 
-        position={[10, 10, 10]} 
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        castShadow
-      />
-      <primitive
-        object={scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[0, 0, 0]}
-      />
-    </mesh>
-  );
-};
-
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-    setIsMobile(mediaQuery.matches);    
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
+const Hero = () => {
   return (
     <>
       <style>
         {`
-          .canvas-container {
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
+          .gradient {
+            background: linear-gradient(to bottom, #524a9f, #0C1844);
           }
-          .canvas {
-            width: 100%;
-            height: 100%;
+          .blink {
+            animation: blink 1s step-end infinite;
           }
-
-          /* Small screens */
-          @media (max-width: 500px) {
-            .canvas-container {
-              width: 100%;
-              height: 60vh;
-              top: 250px;
-              left: 0;
-            }
-            .canvas {
-              width: 100%;
-              height: 100%;
-            }
-          }
-
-          /* Medium screens */
-          @media (min-width: 501px) and (max-width: 1024px) {
-            .canvas-container {
-              width: 100%;
-              height: 80vh;
-              top: 250px;
-              left: 0px;
-            }
-            .canvas {
-              width: 100%;
-              height: 100%;
-            }
-          }
-
-          /* Large screens */
-          @media (min-width: 1025px) {
-            .canvas-container {
-              width: 75%;
-              height: 70vh;
-              top: 250px;
-              left: 150px;
-            }
-            .canvas {
-              width: 100%;
-              height: 100%;
-            }
+          @keyframes blink {
+            from, to { opacity: 1 }
+            50% { opacity: 0 }
           }
         `}
       </style>
-      <div className="canvas-container">
-        <Canvas
-          className="canvas"
-          frameloop='demand'
-          shadows
-          dpr={[1, 2]}
-          camera={{ position: [0, 30, 75], fov: 40 }}
-          gl={{ preserveDrawingBuffer: true }}
-        >
-          <Suspense fallback={<CanvasLoader />}>
-            <OrbitControls
-              enableZoom={false}
+      <section className="relative w-full h-screen mx-auto">
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#524a9f]"/>
+            <div className="w-1 sm:h-80 h-40 gradient"/>
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}><AnimatedText /></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I develop mobile apps📱, <br className='sm:block hidden' />
+              & web apps 💻 
+            </p>
+          </div>
+        </div>
+        <ComputersCanvas />
+        <div className="hidden lg:flex absolute top-1/4 left-2/3 w-[155px] h-[155px] flex justify-center items-start">
+          <div className="w-[100px] h-[100px] rounded-full border-x-4 border-[#83B4FF] flex justify-center p-2">
+            <motion.div
+              initial={{ y: 35 }}
+              animate={{ x: [-25, 40, -25] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
             />
-            <Computers isMobile={isMobile} />
-          </Suspense>
-          <Preload all />
-        </Canvas>
-      </div>
+            <motion.div
+              initial={{ x: -5 }}
+              animate={{ y: [0, 65, 0] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+          </div>
+        </div>
+        <div className="hidden lg:flex absolute top-1/2 end-52 w-[155px] h-[155px] flex justify-center items-start">
+          <div className="w-[100px] h-[100px] rounded-2xl border-y-4 border-[#83B4FF] flex justify-center p-2">
+            <motion.div
+              initial={{ y: 35 }}
+              animate={{ x: [-25, 40, -25] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+            <motion.div
+              initial={{ x: -5 }}
+              animate={{ y: [0, 65, 0] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+          </div>
+        </div>
+        <div className="hidden lg:flex absolute top-1/4 right-20 w-[155px] h-[155px] flex justify-center items-start">
+          <div className="w-[100px] h-[100px] rounded-full border-x-4 border-[#83B4FF] flex justify-center p-2">
+            <motion.div
+              initial={{ y: 35 }}
+              animate={{ x: [-25, 40, -25] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+            <motion.div
+              initial={{ x: -5 }}
+              animate={{ y: [0, 65, 0] }}
+              transition={{ duration: 5, repeat: Infinity, repeatType: 'reverse' }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default ComputersCanvas;
+export default Hero;
